8d4bfd1066e2f985b9f6504afb378f550cc99fdb
Om <126419898+om-modh@users.noreply.github.com>
2023-09-01 16:29:03 +0530

    routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -144,7 +144,7 @@ def update_post(post_id):
         post.content = form.content.data
         db.session.commit()
         flash("Your post has been updated!", "success")
-        return redirect(url_for('post',post_id=post_id))
+        return redirect(url_for('post',post_id=post.id))
     elif request.method == ['GET']:
         form.title.data = post.title
         form.content.data = post.content
